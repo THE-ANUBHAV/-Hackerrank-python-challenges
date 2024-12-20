9c12a990135b2ce40c1facedeb63000eccf9015f
ANUBHAV SINGH <anubhavsingh14122005@gmail.com>
2024-12-20 21:17:36 +0530

    Add to challenges

--- Challenges/append-and-delete.py
+++ Challenges/append-and-delete.py
@@ -11,7 +11,6 @@ import sys
 #  1. STRING s
 #  2. STRING t
 #  3. INTEGER k
-#
 def appendAndDelete(s, t, k):
     i = 0
     while i < min(len(s), len(t)) and s[i] == t[i]:
@@ -23,7 +22,7 @@ def appendAndDelete(s, t, k):
         return "No"     
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
+    
     s = input()
 
     t = input()
